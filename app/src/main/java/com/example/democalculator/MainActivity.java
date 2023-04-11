26e3bf21261c8e801311f2fba6f3b0e00698274d
Thái Tăng Đức <thaitangduc1@gmail.com>
2023-04-11 13:41:55 +0700

    update

--- app/src/main/java/com/example/democalculator/MainActivity.java
+++ app/src/main/java/com/example/democalculator/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,7 +21,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
         etGiatri = (EditText) findViewById(R.id.etResult);
         int[] idButton = {R.id.btn0,R.id.btn1,R.id.btn2,R.id.btn3,R.id.btn4,
                 R.id.btn5,R.id.btn6,R.id.btn7,R.id.btn8,R.id.btn9,R.id.btnPoint,
-                R.id.btnAdd,R.id.btnMul,R.id.btnDel,R.id.btnDev, R.id.btnSub};
+                R.id.btnAdd,R.id.btnMul,R.id.btnDel,R.id.btnDev, R.id.btnSub, R.id.btnResult};
 
         for (int id:idButton) {
             View v = (View)findViewById(id);
@@ -33,6 +34,10 @@ public class MainActivity extends Activity implements View.OnClickListener {
         switch (v.getId()){
             case R.id.btnAdd:
                 sToantu = "+";
+                fThamso1 = Float.parseFloat(etGiatri.getText().toString());
+                sXuatkq = "0";
+                etGiatri.setText("0");
+                Log.d("ketqua","" + fThamso1);
                 break;
             case R.id.btnDev:
                 sToantu = "/";
@@ -45,8 +50,9 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 break;
             case R.id.btnResult:
                 sToantu = "=";
+                fThamso2 = Float.parseFloat(etGiatri.getText().toString());
                 break;
-            default: fThamso1 = Float.parseFloat(etGiatri.getText().toString());
+            default:
             sXuatkq += ((Button)v).getText().toString();
             etGiatri.setText(sXuatkq);
         }
